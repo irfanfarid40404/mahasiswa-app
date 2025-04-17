d118c2050997b7c71201e4db73247f228cf4e7de
irfanfarid40404 <crime14004@gmail.com>
2025-04-17 07:30:32 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8787',
+        target: 'http://localhost:5173',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
